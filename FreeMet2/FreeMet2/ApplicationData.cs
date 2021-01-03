16c7fbd24db666c75bebabe59336443a5a5bf4c4
Mniwyl <wenyule@163.com>
2021-01-03 21:48:23 +0800

    ApplicationData / ApplicationStartup

---
+++ FreeMet2/FreeMet2/ApplicationData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeMet2
+{
+    public class ApplicationData
+    {
+        public ApplicationData()
+        {
+            
+        }
+        private string _token;
+        public string Token
+        {
+            get => _token;
+            set => _token = value;
+        }
+
+        private DateTime _getTokenTime;
+        public DateTime GetTokenTime
+        {
+            get => _getTokenTime;
+            set => _getTokenTime = value;
+        }
+
+        private int _expireTime;
+        public int ExpireTime
+        {
+            get => _expireTime;
+            set => _expireTime = value;
+        }
+
+        private string _userName;
+        public string UserName
+        {
+            get => _userName;
+            set => _userName = value;
+        }
+    }
+}
---
+++ FreeMet2/FreeMet2/ApplicationStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FreeMet2
+{
+    public class ApplicationStartup : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            CheckSingleInstance();
+        }
+
+        private void CheckSingleInstance()
+        {
+            var singleInstanceMutex = new Mutex(true, this.MainWindow.Name, out bool singleInstance);
+            if(!singleInstance)
+            {
+                return;
+            }
+
+            KillProcess();
+        }
+
+        private void KillProcess()
+        {
+
+        }
+    }
+}
